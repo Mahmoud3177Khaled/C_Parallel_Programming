df30cbbfec32141a2eecf93914b30fc38b0ec117
Mahmoud Khaled <mahmoudkhaled3177@gmail.com>
2025-05-02 11:26:17 +0300

    openmp partice + a2t1 working, but partialy done

---
+++ A2_20220317/t1_bcast_reduce.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+#include"mpi.h"
+#include<math.h>
+
+int main(int argc, char** argv) {
+
+    // mpi variables
+    int my_rank;
+    int p;
+    int tag = 0;
+    MPI_Status status;
+    
+    // problem variables
+    int partition = 0;
+    int my_count = 0;
+    int total_count = 0;
+    int x = 0;
+    int y = 0;
+    
+    
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    
+    if(my_rank == 0) {
+        
+
+        printf("Hello from master process.\n");
+
+        // printf("Enter start x: \n");
+        // scanf("%d", &x);
+        
+        // printf("Enter end y: \n");
+        // scanf("%d", &y);
+
+        x = 1;
+        y = 10;
+
+        partition = floor((double)((y-x)+1)/(p-1));
+        
+        // printf("partition size is: %d", partition);
+        
+        
+    } 
+    
+    MPI_Bcast(&partition, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&x, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&y, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+
+    if(my_rank != 0) {
+        
+        int my_x = x+((my_rank-1)*partition);
+        int my_y = x+(my_rank*partition)-1;
+        
+        if(my_rank == p-1) {
+            my_y += ((y-x)+1)%(p-1);
+
+        }
+
+        for (int i = my_x; i <= my_y; i++) {
+            int prime = 1;
+
+            if(i <= 1) {
+                continue;
+            }
+
+            if(i == 2) {
+                my_count++;
+                continue;
+            }
+
+            for (int j = 2; j <= (int)sqrt(i); j++) {
+
+                if(i%j == 0) {
+                    prime = 0;
+                    break;
+                }
+                
+            }
+
+            if(prime) {
+                my_count++;
+                // printf("%d -> ", i);
+
+            }
+        }
+
+        printf("slave%d: counting primes from %d to %d .... count: %d.\n", my_rank, my_x, my_y, my_count); 
+        
+        
+
+    }
+
+
+    MPI_Reduce(&my_count, &total_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if(my_rank == 0) {
+        printf("\n Master announces total prime numbers count is: %d\n", total_count);
+
+    }
+
+
+    MPI_Finalize();
+}
+
+
